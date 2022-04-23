decd65057f970656b3c0ff52973e06736fbf4730
TheTripleV <vasistavovveti@gmail.com>
2022-04-23 10:59:27 -0500

    add missing button api

--- commands2/button/button.py
+++ commands2/button/button.py
@@ -4,3 +4,7 @@ from ..trigger import Trigger
 class Button(Trigger):
     whenPressed = Trigger.whenActive
     whenReleased = Trigger.whenInactive
+    whileHeld = Trigger.whileActiveContinous
+    whenHeld = Trigger.whileActiveOnce
+    toggleWhenPressed = Trigger.toggleWhenActive
+    cancelWhenPressed = Trigger.cancelWhenActive
